bab43a2c2755fcc9444e6bf220672c4af0ad2548
maverickbustillo5@gmail.com <maverickbustillo5@gmail.com>
2021-04-11 19:53:20 -0600

    estructura frontend lista para adaptacion

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = express();
+var cors = require('cors');
 var bodyParser = require('body-parser');
 
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(cors());
+
 
 app.listen(8888, function(){
     console.log("Servidor Levantado");
