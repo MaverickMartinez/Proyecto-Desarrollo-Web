eb936b81d61897959342d9144dc87607d1ec6d1c
maverickbustillo5@gmail.com <maverickbustillo5@gmail.com>
2021-04-12 18:27:51 -0600

    creacion de formsGroups para formularios

--- frontend/proyectoDW/src/app/components/editar-perfil/editar-perfil.component.ts
+++ frontend/proyectoDW/src/app/components/editar-perfil/editar-perfil.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-editar-perfil',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./editar-perfil.component.css']
 })
 export class EditarPerfilComponent implements OnInit {
-
+  formularioEditarPerfil = new FormGroup({
+    nombre: new FormControl('',[Validators.required, Validators.maxLength(30)]),
+    correoEditar: new FormControl('',[Validators.required, Validators.pattern(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)]),
+    claveEditar1: new FormControl('',[Validators.required, Validators.maxLength(30)]),
+    claveEditar2: new FormControl('',[Validators.required, Validators.maxLength(30)]),
+    nuevoPlan: new FormControl('',[Validators.required])
+  
+  });
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
 }
--- frontend/proyectoDW/src/app/components/registro-usuarios/registro-usuarios.component.ts
+++ frontend/proyectoDW/src/app/components/registro-usuarios/registro-usuarios.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-registro-usuarios',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro-usuarios.component.css']
 })
 export class RegistroUsuariosComponent implements OnInit {
-
+  formularioRegistro = new FormGroup({
+    correoRegistro: new FormControl('',[Validators.required, Validators.pattern(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)]),
+    claveRegistro1: new FormControl('',[Validators.required, Validators.maxLength(30)]),
+    claveRegistro2: new FormControl('',[Validators.required, Validators.maxLength(30)])
+  
+  });
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
 }
